6faaad31c6d6b7222ebf89856750bbe4fe8ecc34
Most <foremost545@gmail.com>
2024-03-10 18:37:04 +0700

    Remove model unused

--- app/src/main/kotlin/com/most/products/application/data/model/Err.kt
+++
@@ -1,8 +0,0 @@
-package com.most.products.application.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Err(
-    @SerializedName("message")
-    var message: String = "",
-)
--- app/src/main/kotlin/com/most/products/application/data/repository/DepartmentsRepositoryImpl.kt
+++ app/src/main/kotlin/com/most/products/application/data/repository/DepartmentsRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package com.most.products.application.data.repository
 
-import com.google.gson.Gson
 import com.most.products.application.core.network.model.ErrorException
 import com.most.products.application.data.api.ApiService
 import com.most.products.application.data.model.DepartmentResponse
-import com.most.products.application.data.model.Err
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.Response
@@ -24,12 +22,8 @@ class DepartmentsRepositoryImpl(
                     emit(it)
                 }
             } else {
-                val err: Err? = Gson().fromJson(
-                    response.errorBody()?.charStream(),
-                    Err::class.java
-                )
                 throw ErrorException.ApiErrorException(
-                    msg = err?.message ?: response.errorBody().toString(),
+                    msg = response.errorBody().toString(),
                     statusCode = response.code().toString()
                 )
             }
